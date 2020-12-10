eed551f16b657581a99d7997296d4afcbbf2ed7a
Benjamin Delespierre <benjamin.delespierre@gmail.com>
2020-12-10 20:40:29 +0100

    easier install

--- install.sh
+++ install.sh
@@ -22,23 +22,10 @@ function __is_available {
 # PHP & Composer Packages
 # -----------------------------------------------------------------------------
 #
-function __install_composer {
-    EXPECTED_CHECKSUM="$(wget -q -O - https://composer.github.io/installer.sig)"
-    php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-    ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
-
-    if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]; then
-        >&2 echo 'ERROR: Invalid installer checksum'
-        rm composer-setup.php
-        exit 1
-    fi
-
-    php composer-setup.php --quiet
-    rm composer-setup.php
-}
-
 if ! __is_available composer; then
-    __install_composer
+    curl https://getcomposer.org/installer -o get-composer.php
+    php get-composer.php --quiet
+    rm get-composer.php
 fi
 
 composer global require phpunit/phpunit
@@ -53,12 +40,8 @@ composer global require squizlabs/php_codesniffer
 # Python & pip Packages
 # -----------------------------------------------------------------------------
 #
-function __install_pip {
+if ! __is_available pip; then
     curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
     python3 get-pip.py
     rm get-pip.py
-}
-
-if ! __is_available pip; then
-    __install_pip
 fi
